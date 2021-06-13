b3b2347f68039d54da5c227415849ca4a100bb65
Wilker, Tobias (9QJ6PC) <twilker@phoenixcontact.com>
2021-06-13 07:36:06 +0200

    Use subgraphs.

--- src/Agents.Net.Designer.ViewModel/MicrosoftGraph/Agents/GraphCreator.cs
+++ src/Agents.Net.Designer.ViewModel/MicrosoftGraph/Agents/GraphCreator.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Agents.Net.Designer.Model;
 using Agents.Net.Designer.Model.Messages;
 using Agents.Net.Designer.ViewModel.MicrosoftGraph.Messages;
+using Microsoft.Msagl.Core.Layout;
+using Microsoft.Msagl.Core.Routing;
 using Microsoft.Msagl.Drawing;
+using Edge = Microsoft.Msagl.Drawing.Edge;
+using Node = Microsoft.Msagl.Drawing.Node;
 
 namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
 {
@@ -23,12 +28,29 @@ namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
             CommunityModel model = messageData.TryGet(out ModificationResult modificationResult)
                                        ? modificationResult.Model
                                        : messageData.Get<ModelLoaded>().Model;
-            Graph graph = new();
-            List<Node> messages = AddMessages(model, graph);
+            Graph graph = new()
+            {
+                LayoutAlgorithmSettings =
+                {
+                    PackingMethod = PackingMethod.Columns,
+                    EdgeRoutingSettings = new EdgeRoutingSettings
+                    {
+                        EdgeRoutingMode = EdgeRoutingMode.SugiyamaSplines,
+                        Padding = 5,
+                        PolylinePadding = 3,
+                        BendPenalty = 1,
+                    },
+                    LiftCrossEdges = true,
+                    NodeSeparation = 20,
+                    ClusterMargin = 40
+                }
+            };
+            Dictionary<string, List<Node>> subgraphCollection = new();
+            List<Node> messages = AddMessages(model, graph, CheckSubgraph);
 
             foreach (AgentModel agentModel in model.Agents)
             {
-                AddAgentNode(agentModel, graph);
+                AddAgentNode(agentModel, graph, CheckSubgraph);
                 AddMessageEdges(agentModel.ConsumingMessages, messages, true,
                                 agentModel.Id, graph);
                 AddMessageEdges(agentModel.ProducedMessages, messages, false,
@@ -39,16 +61,104 @@ namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
                                     agentModel.Id, graph, true);
                 }
                 AddEventEdges(agentModel.IncomingEvents ?? Enumerable.Empty<string>(),
-                              true, agentModel.Id, graph);
+                              true, agentModel.Id, graph, CheckSubgraphForEvent);
                 AddEventEdges(agentModel.ProducedEvents ?? Enumerable.Empty<string>(),
-                              false, agentModel.Id, graph);
+                              false, agentModel.Id, graph, CheckSubgraphForEvent);
             }
 
+            CreateSubGraphs(graph, subgraphCollection);
+
             OnMessage(new GraphCreated(graph, messageData));
+
+            void CheckSubgraphForEvent(Node eventToCheck, Guid connectedAgentId)
+            {
+                AgentModel agentModel = model.Agents.First(a => a.Id == connectedAgentId);
+                string ns = Regex.Replace(agentModel.Namespace, @"\.Agents$", string.Empty);
+                
+                if (!string.IsNullOrEmpty(ns))
+                {
+                    AddNodeToSubgraph(eventToCheck, ns);
+                }
+            }
+            
+            void CheckSubgraph(Node nodeToCheck)
+            {
+                string ns;
+                if (nodeToCheck.UserData is AgentModel agent)
+                {
+                    ns = Regex.Replace(agent.Namespace, @"\.Agents$", string.Empty);
+                }
+                else
+                {
+                    ns = Regex.Replace(((MessageModel) nodeToCheck.UserData).Namespace, @"\.Messages$", string.Empty);
+                }
+
+                if (!string.IsNullOrEmpty(ns))
+                {
+                    AddNodeToSubgraph(nodeToCheck, ns);
+                }
+            }
+
+            void AddNodeToSubgraph(Node node, string ns)
+            {
+                if (!subgraphCollection.ContainsKey(ns))
+                {
+                    subgraphCollection.Add(ns, new List<Node>());
+                }
+                subgraphCollection[ns].Add(node);
+            }
+        }
+
+        private void CreateSubGraphs(Graph graph, Dictionary<string,List<Node>> subgraphCollection)
+        {
+            Stack<(string, Subgraph)> createdGraphs = new();
+            foreach (string ns in subgraphCollection.Keys.OrderBy(ns => ns.Count(c => c == '.')))
+            {
+                Subgraph current = new(ns)
+                {
+                    LayoutSettings = graph.LayoutAlgorithmSettings.Clone()
+                };
+                foreach (Node node in subgraphCollection[ns])
+                {
+                    current.AddNode(node);
+                }
+                Subgraph parent = FindSubgraphParent(ns);
+                parent.AddSubgraph(current);
+                createdGraphs.Push((ns, current));
+            }
+
+            Subgraph[] subGraphs = createdGraphs.Select(g => g.Item2).ToArray();
+            foreach (Subgraph subgraph in subGraphs)
+            {
+                IEnumerable<Subgraph> targetGraphs = subgraph.Nodes.SelectMany(n => n.OutEdges)
+                                                             .Select(e => subGraphs.FirstOrDefault(
+                                                                         g => g.Nodes.Contains(e.TargetNode)))
+                                                             .Distinct()
+                                                             .Where(g => g != null)
+                                                             .Where(g => !Equals(g, subgraph));
+                foreach (Subgraph targetGraph in targetGraphs)
+                {
+                    graph.AddEdge(subgraph.Id, targetGraph.Id);
+                }
+            }
+
+            Subgraph FindSubgraphParent(string ns)
+            {
+                Subgraph parent = graph.RootSubgraph;
+                foreach ((string key, Subgraph subgraph) in createdGraphs)
+                {
+                    if (ns.StartsWith(key, StringComparison.Ordinal))
+                    {
+                        parent = subgraph;
+                        break;
+                    }
+                }
+                return parent;
+            }
         }
 
         private void AddEventEdges(IEnumerable<string> events, bool addAsSource, Guid agentModelId,
-                                   Graph graph)
+                                   Graph graph, Action<Node, Guid> checkSubgraph)
         {
             foreach (string @event in events)
             {
@@ -72,6 +182,7 @@ namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
                 }
                 eventNode.Attr.Shape = Shape.Diamond;
                 eventNode.Attr.FillColor = Color.Gray;
+                checkSubgraph(eventNode, agentModelId);
             }
         }
 
@@ -104,7 +215,7 @@ namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
             }
         }
 
-        private static void AddAgentNode(AgentModel agentModel, Graph graph)
+        private static void AddAgentNode(AgentModel agentModel, Graph graph, Action<Node> checkSubgraph)
         {
             Node agentNode = new(agentModel.Id.ToString("D"))
             {
@@ -117,9 +228,10 @@ namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
                 UserData = agentModel
             };
             graph.AddNode(agentNode);
+            checkSubgraph(agentNode);
         }
 
-        private static List<Node> AddMessages(CommunityModel model, Graph graph)
+        private static List<Node> AddMessages(CommunityModel model, Graph graph, Action<Node> checkSubgraph)
         {
             List<Node> messages = new();
             foreach (MessageModel messageModel in model.Messages)
@@ -137,6 +249,7 @@ namespace Agents.Net.Designer.ViewModel.MicrosoftGraph.Agents
 
                 messages.Add(messageNode);
                 graph.AddNode(messageNode);
+                checkSubgraph(messageNode);
             }
 
             foreach (MessageDecoratorModel decoratorModel in model.Messages.OfType<MessageDecoratorModel>())
